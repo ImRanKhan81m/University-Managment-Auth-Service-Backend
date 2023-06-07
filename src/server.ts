61d04a3a4ebc777d0cca60b27d83c74f3f3b4b29
Md Imran Hossen <freelancerimran81@gmail.com>
2023-06-07 18:49:17 +0600

    zod added done

--- src/server.ts
+++ src/server.ts
@@ -1,46 +1,46 @@
 /* eslint-disable no-console */
-import mongoose from 'mongoose'
-import app from './app'
-import config from './config/index'
-import { logger, errorLogger } from './shared/logger'
-import { Server } from 'http'
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config/index';
+import { logger, errorLogger } from './shared/logger';
+import { Server } from 'http';
 
 process.on('uncaughtException', error => {
-  errorLogger.error(error)
-  process.exit(1)
-})
+  errorLogger.error(error);
+  process.exit(1);
+});
 
-let server: Server
+let server: Server;
 
 async function bootstrap() {
   try {
-    await mongoose.connect(config.databaseURL as string)
-    logger.info('Database connected😎')
+    await mongoose.connect(config.databaseURL as string);
+    logger.info('Database connected😎');
 
     server = app.listen(config.port, () => {
-      logger.info(`Application is listening on port ${config.port}`)
-    })
+      logger.info(`Application is listening on port ${config.port}`);
+    });
   } catch (error) {
-    errorLogger.error('Failed to connect database 😖', error)
+    errorLogger.error('Failed to connect database 😖', error);
   }
 
   process.on('unhandledRejection', error => {
     if (server) {
       server.close(() => {
-        errorLogger.error(error)
-        process.exit(1)
-      })
+        errorLogger.error(error);
+        process.exit(1);
+      });
     } else {
-      process.exit(1)
+      process.exit(1);
     }
-  })
+  });
 }
 
-bootstrap()
+bootstrap();
 
 process.on('SIGTERM', () => {
-  logger.info('SIGTERM is received')
+  logger.info('SIGTERM is received');
   if (server) {
-    server.close()
+    server.close();
   }
-})
+});
