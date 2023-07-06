4490d63e875665049155d89e5dc658e4f6bc8a1e
Md Imran Hossen <freelancerimran81@gmail.com>
2023-07-06 09:40:24 +0600

    auth add

---
+++ src/app/modules/auth/auth.route.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+// import validateRequest from '../../middlewares/validateRequest';
+
+const router = express.Router();
+
+// router.post('/login', validateRequest(AuthValidation.loginZodSchema) )
+
+export const AuthRoutes = router;
